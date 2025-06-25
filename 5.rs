7ef51b04918c4407cf0212e4e55cdc2104717c00
indira345 <129744618+indira345@users.noreply.github.com>
2025-06-25 11:52:47 +0530

    Create 5.rs

---
+++ 5.rs
@@ -0,0 +1,12 @@
+/*
+5. Write a program to implement the following
+a. Implicit type declaration
+b. Explicit type declaration
+*/
+
+fn main() {
+    let implicit_int = 10;
+    let explicit_int: i32 = 20;
+    println!("implicit int = {}", implicit_int);
+    println!("explicit int = {}", explicit_int);
+}
