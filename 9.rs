ce7d02774dc5addc7ef04701e9f76907955df622
indira345 <129744618+indira345@users.noreply.github.com>
2025-06-25 12:02:23 +0530

    Create 9.rs

---
+++ 9.rs
@@ -0,0 +1,16 @@
+/*
+9. Write a program to create different types of constants print it in the output
+*/
+
+const S: &str = "rust";
+const C: char = 'r';
+const A: i32 = 15;
+const F: f64 = 23.59;
+const B: bool = true;
+fn main() {
+    println!("S = {}", S);
+    println!("C = {}", C);
+    println!("A = {}", A);
+    println!("F = {}", F);
+    println!("B = {}", B);
+}
