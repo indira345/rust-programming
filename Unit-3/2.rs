1e032c45a9939013c7d8fd921967ac43789b1b23
indira345 <129744618+indira345@users.noreply.github.com>
2025-07-02 12:22:51 +0530

    Create 2.rs

---
+++ Unit-3/2.rs
@@ -0,0 +1,13 @@
+/*
+2. Write a program to determine if a number is even or odd
+*/
+
+fn main() {
+    let a = 20;
+    if a % 2 == 0 {
+        println!("Even");
+    }
+    else {
+        println!("Odd");
+    }
+}
