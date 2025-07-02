3ff1bea5b5a623a69e5205691245944d59dde10d
indira345 <129744618+indira345@users.noreply.github.com>
2025-07-02 12:21:26 +0530

    Create 1.rs

---
+++ Unit-3/1.rs
@@ -0,0 +1,13 @@
+/*
+1. Write a program to check if a number is positive or negative
+*/
+
+fn main() {
+    let a = -5;
+    if a < 0 {
+        println!("Negative");
+    }
+    else {
+        println!("Positive");
+    }
+}
