26d2edee7d7df9f9306446d2b19dc8d05037408d
indira345 <129744618+indira345@users.noreply.github.com>
2025-07-16 10:14:43 +0530

    Update 1.rs

--- Unit-2/1.rs
+++ Unit-2/1.rs
@@ -2,3 +2,9 @@
 1. Write a program to implement Type Casting Operator.
 */
 
+fn main() {
+    let f: i32 = 24;
+    let t = (f as f64)/3.0;
+    println!("f = {}", f);
+    println!("t = {}", t);
+}
