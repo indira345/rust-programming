5cc0f3cf7e5a09b7c4e68c136ec7da7292848149
indira345 <129744618+indira345@users.noreply.github.com>
2025-07-16 10:19:56 +0530

    Create 2.rs

---
+++ Unit-2/2.rs
@@ -0,0 +1,19 @@
+/*
+2. Write a program to implement Borrowing and Dereferencing Operators
+*/
+
+fn main() {
+    // immutable borrow
+    let a = 10;
+    let b = &a;
+    println!("a = {}", a);
+    println!("b = {}", b);
+    
+    // mutable borrow
+    let mut p = 20;
+    let q = &mut p;
+    println!("p = {}", q);
+    *q = 30;
+    println!("p = {}", q);
+    
+}
