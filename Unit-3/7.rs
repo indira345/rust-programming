c8bf6931ea9e1e44551f02754c5dbc1c2ec618b0
indira345 <129744618+indira345@users.noreply.github.com>
2025-08-29 09:41:52 +0530

    Create 7.rs

---
+++ Unit-3/7.rs
@@ -0,0 +1,10 @@
+/*
+7. Write a program to Count Iterations of a Loop Until a Condition
+*/
+fn main() {
+    let mut count = 0;
+    while count <= 5 {
+        count = count + 1;
+    }
+    println!("No.of iterations = {}", count);
+}
