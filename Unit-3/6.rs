24aa78c962664968e1e611062dd99cfea9fea121
indira345 <129744618+indira345@users.noreply.github.com>
2025-08-29 09:35:53 +0530

    Update 6.rs

--- Unit-3/6.rs
+++ Unit-3/6.rs
@@ -1,3 +1,11 @@
 /*
 6. Write a program to Multiplication Table using Loop Labels
 */
+fn main() {
+ for i in 1..5{ //outer loop
+  println!("Multiplication Table of : {}", i);
+   for j in 1..11 { // inner loop
+       println!("{} * {} = {}", i, j, i * j);
+   }
+ }
+}
